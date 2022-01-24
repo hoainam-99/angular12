17adeac4a21070355886cd76ec8094b361d6fbda
hoainam-99 <hoainam12121999@gmail.com>
2022-01-24 10:56:56 +0700

    proporties

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  public name = 'Nam'
+  public name = 'Hoài Nam'
   constructor() { }
 
   ngOnInit(): void {
