85c4ecc1522799fde7138f447fb44787eb61be49
hoainam-99 <hoainam12121999@gmail.com>
2022-01-24 15:17:26 +0700

    fruit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,25 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   public name = 'Hoài Nam';
   public age = 10;
+  public traiCay = [
+    {
+      name: "Xoài",
+      price: 12,
+      sale: true
+    },{
+      name: "Cam",
+      price: 15,
+      sale: false
+    },{
+      name: "Bưởi",
+      price: 20,
+      sale: true
+    },{
+      name: "Nho",
+      price: 40,
+      sale: false
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
