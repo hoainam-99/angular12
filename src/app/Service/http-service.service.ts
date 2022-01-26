fcfee23fd57207f3f49acf9f85544373ea0af2e1
hoainam-99 <hoainam12121999@gmail.com>
2022-01-26 19:55:19 +0700

    httpGet method

---
+++ src/app/Service/http-service.service.ts
@@ -0,0 +1,31 @@
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpServiceService {
+
+  private REST_API_SERVER = 'http://localhost:3000';
+  private REST_API_SERVER_RANDOM_USERS = 'https://randomuser.me/api/?results=';
+  private httpOption = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+
+    })
+  }
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getComments():Observable<any>{
+    const url = `${this.REST_API_SERVER}/stdList`;
+    return this.httpClient.get<any>(url, this.httpOption);
+  }
+  
+  public getRandomUsers(n: number = 1):Observable<any>{
+    const url = `${this.REST_API_SERVER_RANDOM_USERS}` + n;
+    console.log(url);
+    return this.httpClient.get<any>(url, this.httpOption);
+  }
+}
